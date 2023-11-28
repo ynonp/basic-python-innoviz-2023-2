5314aee5e8de2c826b40404fd577b2ae74919168
ynonp <ynonperek@gmail.com>
2023-11-28 13:20:34 +0200

    add solutions

--- exercises2.py
+++ exercises2.py
@@ -1,4 +1,6 @@
 # Exercises
+# Project:
+# https://github.com/ynonp/basic-python-innoviz-2023-2
 #
 # https://projecteuler.net/problem=1
 # Answer: 233168
@@ -9,3 +11,29 @@
 #
 # https://projecteuler.net/problem=6
 # Answer: 25164150
+#
+# 3, 5, 6, 9
+total = 0
+for i in range(1_000):
+    if (i % 3 == 0) or (i % 5 == 0):
+        # print(f"{i} is a multiplication of 3 or 5")
+        total += i
+
+print("ex1:")
+print(total)
+
+max_product = 0
+for i in range(100, 1_000):
+    for j in range(100, 1_000):
+        product = i * j
+        if str(product) == str(product)[::-1]:
+            if product > max_product:
+                max_product = product
+
+print(max_product)
+
+
+
+
+
+
