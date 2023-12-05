95a301aba68e2471d780d13e0d056a55ae773f80
ynonp <ynonperek@gmail.com>
2023-12-05 10:15:35 +0200

    euler 6 solution

---
+++ day2/euler6.py
@@ -0,0 +1,20 @@
+# https://github.com/ynonp/basic-python-innoviz-2023-2
+
+# def - define a new function
+# def function-name():
+#   body
+#   body
+def euler6(last_number_in_sum: int):
+    total = 0
+    total_squares = 0
+    for i in range(1, last_number_in_sum + 1):
+        total += i
+        total_squares += i ** 2
+
+    square_total = total ** 2
+    print(square_total - total_squares)
+
+
+euler6(10)
+euler6(100)
+euler6(1000)
